03cedb8452e61706f3c02d938e44521a5ef21003
yuliang <yu.liang@freelog.com>
2018-12-25 16:01:32 +0800

    1.新增dockerfile

--- app/router.js
+++ app/router.js
@@ -2,9 +2,15 @@
 
 module.exports = app => {
 
+    //首页
     app.router.all('/', 'home.index')
+
+    //同步路由信息
+    app.router.get("/gateway/routers/syncRouterData", "router.syncRouterData")
+
+    //restful
     app.router.resources("router", "/gateway/routers", "router")
 
-    app.router.all('/*', (ctx, next) => {
-    })
+    //其他默认当网关代理处理
+    app.router.all('/*', (ctx, next) => null)
 }
