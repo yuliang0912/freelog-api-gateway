5f9a59700c755e0debd6700d1877d1219609555c
yuliang <yu.liang@freelog.com>
2019-02-22 15:26:36 +0800

    1.取消API格式校验

--- app/middleware/proxy-response-handler.js
+++ app/middleware/proxy-response-handler.js
@@ -14,19 +14,20 @@ class ComponentHandler {
      */
     async main(ctx, next) {
 
-
         const {proxyResponse} = ctx
         const {headers, statusCode, body} = proxyResponse
 
+        if (statusCode >= 500 && statusCode < 600) {
+            ctx.error({msg: "上游API服务器异常", data: {proxy: ctx.proxyInfo, body: body.toString()}})
+        }
         /**
          * 目前freelog-api服务限定只返回json或者附件文件两种.
          * 后续如果有其他需求可以拓展,或者直接在路由信息中进行格式检查配置
          * 此处暂未考虑304等.
          */
-        if (!this._isJson(headers) && !this._isAttachment(headers)) {
-            ctx.status = 500
-            ctx.error({msg: "上游API未按协议返回数据格式", data: {proxy: ctx.proxyInfo, body: body.toString()}})
-        }
+        // else if (!this._isJson(headers) && !this._isAttachment(headers)) {
+        //     ctx.error({msg: "上游API未按协议返回数据格式", data: {proxy: ctx.proxyInfo, body: body.toString()}})
+        // }
 
         ctx.body = body
         ctx.status = statusCode
