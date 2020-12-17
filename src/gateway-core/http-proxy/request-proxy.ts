2a6d9ee2e4746b6cd49cea0eedcebcaeca89b2a3
yuliang <yu.liang@freelog.com>
2020-12-17 14:40:34 +0800

    1.优化代码

--- src/gateway-core/http-proxy/request-proxy.ts
+++ src/gateway-core/http-proxy/request-proxy.ts
@@ -49,6 +49,9 @@ export class HttpRequestProxy implements IHttpRequestProxy {
             if (ctx.req.readable && !["GET", "HEAD", "DELETE"].includes(options.method)) {
                 ctx.req.pipe(proxyServer);
             }
+            if (ctx.res.writable) {
+                proxyServer.pipe(ctx.res);
+            }
         })
 
         //暂不启用直接pipe到ctx.res方案,一般大文件传输才需要考虑
