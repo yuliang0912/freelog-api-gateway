95dec15c3b1d419490e3daab652e2be67ff31f96
yuliang <4896819@qq.com>
2022-01-24 11:38:04 +0800

    1.提交代码

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -4,8 +4,12 @@ export default (appInfo: EggAppInfo) => {
 
     const config: any = {};
 
+    // config.mongoose = {
+    //     url: decodeURIComponent('mongodb%3A%2F%2Froot%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-private.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-private-secondary.mongodb.rds.aliyuncs.com%3A3717%2Ffreelog-api-gateway%3FreplicaSet%3Dmgset-58730021')
+    // };
+
     config.mongoose = {
-        url: decodeURIComponent('mongodb%3A%2F%2Froot%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-private.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-private-secondary.mongodb.rds.aliyuncs.com%3A3717%2Ffreelog-api-gateway%3FreplicaSet%3Dmgset-58730021')
+        url: decodeURIComponent(`mongodb%3A%2F%2Fgateway_service%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-public.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-public-secondary.mongodb.rds.aliyuncs.com%3A3717%2Ffreelog-api-gateway%3FreplicaSet%3Dmgset-58730021`)
     };
 
     return config;
